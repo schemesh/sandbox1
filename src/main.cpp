03c44f39930e8e73c3952919d179efd93f72e5cb
Schemesh Sunny <sschemesh@bk.ru>
2018-10-31 20:08:50 +0200

    add temperature measureing module

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
+#include <temperature.h>
+
+byte idt1,idt2;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(4,OUTPUT);
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  idt1 = temperatureSetupSensor(4);
+  idt2 = temperatureSetupSensor(5);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(4,HIGH);
-  delay(200);
-  digitalWrite(4,LOW);
-  delay(200);
+  temperatureStartMeasure(idt1);
+  temperatureStartMeasure(idt2);
+  delay(1000);
+  Serial.print("Sensor 1: ");
+  Serial.println(temperatureRead(idt1));
+  Serial.print("Sensor 2: ");
+  Serial.println(temperatureRead(idt2));
 }
